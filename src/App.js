b55288009b49a18413379bac26c807652934651f
IdrisGuloyev <idrisguloyev@gmail.com>
2022-08-18 20:59:59 +0600

    Организация Файлов

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import CostItem from "./components/CostItem";
+import Costs from "./components/Costs/Costs";
 
-function App() {
+const App = () => {
   const costs = [
     {
       date: new Date(2021, 2, 12),
@@ -22,21 +22,7 @@ function App() {
   return (
     <div>
       <h1>Начнем изучение React!</h1>
-      <CostItem
-        date={costs[0].date}
-        description={costs[0].description}
-        amount={costs[0].amount}
-      />
-      <CostItem
-        date={costs[1].date}
-        description={costs[1].description}
-        amount={costs[1].amount}
-      />
-      <CostItem
-        date={costs[2].date}
-        description={costs[2].description}
-        amount={costs[2].amount}
-      />
+      <Costs costs={costs} />
     </div>
   );
 }
