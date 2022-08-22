c8730f578d8a42e739c729901545d2020c2ca895
IdrisGuloyev <idrisguloyev@gmail.com>
2022-08-22 22:16:22 +0600

    Добавление Двустороннего Связывания

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -1,26 +1,88 @@
-import './CostForm.css';
+import React, { useState } from "react";
+import "./CostForm.css";
 
 const CostForm = () => {
-    return <form>
-        <div className="new-cost__controls">
-            <div className="new-cost__control">
-                <label>Название</label>
-                <input type='text' />
-            </div>
-            <div className="new-cost__control">
-                <label>Сумма</label>
-                <input type='number' min='0.01' step='0.01'/>
-            </div>
-            <div className="new-cost__control">
-                <label>Дата</label>
-                <input type='date' min='2019-01-01' step='0.2022-12-31'/>
-            </div>
-            <div className="new-cost__actions">
-                <button type="submit">Добавить расход</button>
+  const [inputName, setInputName] = useState("");
+  const [inputAmount, setInputAmount] = useState("");
+  const [inputDate, setInputDate] = useState("");
 
-            </div>
+  //   const [userInput, setUserInput] = useState({
+  //     name: "",
+  //     amount: "",
+  //     date: "",
+  //   });
+
+  const nameChangeHandler = (event) => {
+    setInputName(event.target.value);
+    // // setUserInput({
+    // //   ...userInput,
+    // //   name: event.target.value,
+    // // });
+  };
+
+  const amountChangeHandler = (event) => {
+    setInputAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   amount: event.target.value,
+    // });
+  };
+
+  const dateChangeHandler = (event) => {
+    setInputDate(event.target.value);
+    // setUserInput({
+    //     ...userInput,
+    //     date: event.target.value,
+    //   });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const costData = {
+      name: inputName,
+      amount: inputAmount,
+      date: new Date(inputDate),
+    };
+
+    setInputName("");
+    setInputAmount("");
+    setInputDate("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-cost__controls">
+        <div className="new-cost__control">
+          <label>Название</label>
+          <input type="text" value={inputName} onChange={nameChangeHandler} />
         </div>
+        <div className="new-cost__control">
+          <label>Сумма</label>
+          <input
+            value={inputAmount}
+            onChange={amountChangeHandler}
+            type="number"
+            min="0.01"
+            step="0.01"
+          />
+        </div>
+        <div className="new-cost__control">
+          <label>Дата</label>
+          <input
+            value={inputDate}
+            onChange={dateChangeHandler}
+            type="date"
+            min="2019-01-01"
+            step="0.2022-12-31"
+          />
+        </div>
+        <div className="new-cost__actions">
+          <button type="submit">Добавить Расход</button>
+        </div>
+      </div>
     </form>
-}
+  );
+};
 
 export default CostForm;
