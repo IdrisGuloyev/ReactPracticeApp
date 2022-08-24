12e5114ed3311face3d2418a60b0ccf5dbb0e27c
IdrisGuloyev <idrisguloyev@gmail.com>
2022-08-24 21:22:54 +0600

    Использование Списков с Состоянием

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
+import React, { useState } from "react";
 import NewCost from "./components/NewCost/NewCost";
 import Costs from "./components/Costs/Costs";
 
+const INITIAL_COSTS = [
+  {
+    id: "C1",
+    date: new Date(2021, 2, 12),
+    description: "Холодильник",
+    amount: 999.99,
+  },
+  {
+    id: "C2",
+    date: new Date(2021, 11, 25),
+    description: "MacBook",
+    amount: 1254.72,
+  },
+  {
+    id: "C3",
+    date: new Date(2021, 3, 1),
+    description: "Джинсы",
+    amount: 49.99,
+  },
+];
+
 const App = () => {
-  const costs = [
-    {
-      id: 'C1',
-      date: new Date(2021, 2, 12),
-      description: "Холодильник",
-      amount: 999.99,
-    },
-    {
-      id: 'C2',
-      date: new Date(2021, 11, 25),
-      description: "MacBook",
-      amount: 1254.72,
-    },
-    {
-      id: 'C3',
-      date: new Date(2021, 3, 1),
-      description: "Джинсы",
-      amount: 49.99,
-    },
-  ];
+  const [costs, setCosts] = useState(INITIAL_COSTS);
 
   const addCostHandler = (cost) => {
-
-  }
+    setCosts((prevCosts) => {
+      return [cost, ...prevCosts];
+    });
+  };
   return (
     <div>
-      <NewCost onAddCost={addCostHandler}/>
+      <NewCost onAddCost={addCostHandler} />
       <Costs costs={costs} />
     </div>
   );
--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,16 +1,8 @@
 import "./CostItem.css";
 import CostDate from "./CostDate";
 import Card from "../UI/Card";
-import React, { useState } from "react";
 
 const CostItem = (props) => {
-  const [description, setDescription] = useState(props.description);
-
-  const changeDescriptionHandler = () => {
-    setDescription("New cost");
-    console.log(description);
-  };
-
   return (
     <Card className="cost-item">
       <CostDate date={props.date} />
@@ -18,7 +10,6 @@ const CostItem = (props) => {
         <h2>{props.description}</h2>
         <div className="cost-item__price">${props.amount}</div>
       </div>
-      <button onClick={changeDescriptionHandler}>Изменить описание </button>
     </Card>
   );
 };
