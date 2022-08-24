59ec16d8a34b557fcb4ffa3fd4d0c498d28472f9
IdrisGuloyev <idrisguloyev@gmail.com>
2022-08-24 23:14:32 +0600

    Возвращение Значения по Условию

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,8 +1,9 @@
-import CostItem from "./CostItem";
+
 import "./Costs.css";
 import Card from "../UI/Card";
 import CostsFilter from "./CostsFilter";
 import React, { useState } from "react";
+import CostList from "./CostList";
 
 const Costs = (props) => {
   const [selectedYear, setSelectedYear] = useState("2021");
@@ -15,18 +16,14 @@ const Costs = (props) => {
     return cost.date.getFullYear().toString() === selectedYear;
   });
 
+  
+
   return (
     <div>
       <Card className="costs">
         <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
-        {filteredCosts.map((cost) => (
-          <CostItem
-            key={cost.id}
-            date={cost.date}
-            description={cost.description}
-            amount={cost.amount}
-          />
-        ))}
+
+        <CostList costs={filteredCosts}/>
       </Card>
     </div>
   );
