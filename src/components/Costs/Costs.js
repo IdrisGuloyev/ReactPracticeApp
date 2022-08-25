96ec5826b334385a8cfd2776dc1c1c85256284c9
IdrisGuloyev <idrisguloyev@gmail.com>
2022-08-25 21:45:54 +0600

    Динамические Стили

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,9 +1,9 @@
-
 import "./Costs.css";
 import Card from "../UI/Card";
 import CostsFilter from "./CostsFilter";
 import React, { useState } from "react";
 import CostList from "./CostList";
+import CostsDiagram from "./CostsDiagram";
 
 const Costs = (props) => {
   const [selectedYear, setSelectedYear] = useState("2021");
@@ -16,14 +16,12 @@ const Costs = (props) => {
     return cost.date.getFullYear().toString() === selectedYear;
   });
 
-  
-
   return (
     <div>
       <Card className="costs">
         <CostsFilter year={selectedYear} onChangeYear={yearChangeHandler} />
-
-        <CostList costs={filteredCosts}/>
+        <CostsDiagram costs={filteredCosts} />
+        <CostList costs={filteredCosts} />
       </Card>
     </div>
   );
